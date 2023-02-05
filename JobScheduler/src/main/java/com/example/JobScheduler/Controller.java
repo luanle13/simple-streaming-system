25c0b159bc05ef511f2e9b5332157ef83c2feebc
luanle13 <thanhluan130201@gmail.com>
2023-02-06 04:27:00 +0700

    semi-rollback

---
+++ JobScheduler/src/main/java/com/example/JobScheduler/Controller.java
@@ -0,0 +1,73 @@
+package com.example.JobScheduler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    private String _password = "admin";
+    private Map<String, ServerSocket> _mapServerSocket = new HashMap<String, ServerSocket>();
+    private volatile static String _message = null;
+    private ServerSocket findServerSocketWithPort(int port) throws IOException {
+        for (Map.Entry<String,ServerSocket> item : _mapServerSocket.entrySet()) {
+            if (Integer.toString(port).equals(item.getKey())) {
+                return item.getValue();
+            }
+        }
+        ServerSocket newServerSocket = new ServerSocket(port);
+        _mapServerSocket.put(Integer.toString(port), newServerSocket);
+        return newServerSocket;
+    }
+    private boolean isPortInUse(int port) {
+        for (Map.Entry<String, ServerSocket> item : _mapServerSocket.entrySet()) {
+            if (Integer.toString(port).equals(item.getKey())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @PostMapping("/")
+    public ResponseEntity<String> handleCreateSocketServer(@RequestBody Authentication body) {
+        if (!_password.equals(body.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        } else {
+            try {
+                ServerSocket serverSocket = findServerSocketWithPort(body.getPort());
+                System.out.println(_mapServerSocket);
+                new Thread(() -> {
+                    try {
+                        Socket socket = serverSocket.accept();
+                        if (body.getType().equals("client")) {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            while (true) {
+                                _message = reader.readLine();
+                            }
+                        }
+                        if (body.getType().equals("server")) {
+                            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                            while (true) {
+                                if (_message != null) {
+                                    writer.println(_message);
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+                return ResponseEntity.status(HttpStatus.OK).build();
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        }
+    }
+}
